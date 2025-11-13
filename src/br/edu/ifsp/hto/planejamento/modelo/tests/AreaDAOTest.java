86b746d8d9f5db3866487f0848a59af9343f0df2
Otávio <113644590+otavio61@users.noreply.github.com>
2025-11-13 00:36:33 -0300

    Refatoração de AreaDAOTest
    Testagem dos métodos do DAO através da classe AreaControle

--- src/br/edu/ifsp/hto/planejamento/modelo/tests/AreaDAOTest.java
+++ src/br/edu/ifsp/hto/planejamento/modelo/tests/AreaDAOTest.java
@@ -2,26 +2,26 @@ package br.edu.ifsp.hto.planejamento.modelo.tests;
 
 import java.util.List;
 
-import br.edu.ifsp.hto.planejamento.modelo.DAO.AreaDAO;
+import br.edu.ifsp.hto.planejamento.controle.AreaControle;
 import br.edu.ifsp.hto.planejamento.modelo.VO.AreaComTalhoesVO;
 import br.edu.ifsp.hto.planejamento.modelo.VO.AreaVO;
 
 public class AreaDAOTest {
     public static void main(String[] args) throws Exception {
-        AreaDAO areaDAO = new AreaDAO();
+        AreaControle areaC = new AreaControle();
 
-        // areaDAO.inserir(new AreaVO(1, "Area Daora", 1000, 0, 7));
+        // areaC.inserir(new AreaVO(1, "Area Daora", 1000, 0, 7));
 
-        List<AreaVO> areas = areaDAO.listarTodas();
+        List<AreaVO> areas = areaC.listarTodas();
         areas.forEach(area -> System.out.println(area.getNome()));
 
-        AreaComTalhoesVO at = areaDAO.buscarAreaComTalhoes(1);
-        System.out.println(at.getTalhoes().get(0).getNome());
+        AreaComTalhoesVO at = areaC.buscarAreaComTalhoes(1);
+        at.getTalhoes().forEach(t -> System.out.println(t.getNome()));
 
-        AreaVO mudar = areas.get(0);
-        mudar.setNome("FAZENDA SUL DAORA");
-        areaDAO.atualizar(mudar);
+        // AreaVO mudar = areas.get(0);
+        // mudar.setNome("FAZENDA SUL DAORA");
+        // areaC.atualizar(mudar);
 
-        areaDAO.deletar(2);
+        // areaC.deletar(2);
     }
 }
