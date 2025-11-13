aa4d2de9680b69087dae1d2f45d9963e1d2f5df4
Otávio <113644590+otavio61@users.noreply.github.com>
2025-11-13 00:42:03 -0300

    Refatoração de CanteiroDAO
    Testagem dos métodos do DAO através de CanteiroControle

--- src/br/edu/ifsp/hto/planejamento/modelo/tests/CanteiroDAOTest.java
+++ src/br/edu/ifsp/hto/planejamento/modelo/tests/CanteiroDAOTest.java
@@ -2,20 +2,22 @@ package br.edu.ifsp.hto.planejamento.modelo.tests;
 
 import java.util.List;
 
-import br.edu.ifsp.hto.planejamento.modelo.DAO.CanteiroDAO;
+import br.edu.ifsp.hto.planejamento.controle.CanteiroControle;
 import br.edu.ifsp.hto.planejamento.modelo.VO.CanteiroComAtividadesVO;
 import br.edu.ifsp.hto.planejamento.modelo.VO.CanteiroVO;
 
 public class CanteiroDAOTest {
     public static void main(String[] args) {
-        CanteiroDAO canteiroDAO = new CanteiroDAO();
-        List<CanteiroVO> canteiros = canteiroDAO.listarTodos();
+        CanteiroControle canteiroC = new CanteiroControle();
+        List<CanteiroVO> canteiros = canteiroC.listarTodos();
         canteiros.forEach(canteiro -> System.out.println(canteiro.getNome()));
 
         CanteiroVO novo = new CanteiroVO(3, "Canteiro Daora", 1000, "canteiro muito daora", 3000);
-        // canteiroDAO.inserir(novo);
+        // canteiroC.inserir(novo);
 
-        CanteiroComAtividadesVO ca = canteiroDAO.buscarCanteiroComAtividades(1);
+        CanteiroComAtividadesVO ca = canteiroC.buscarCanteiroComAtividades(1);
         ca.getAtividades().forEach(a -> System.out.println(a.getDataAtividade()));
+
+        // canteiroC.deletar(2);
     }
 }
