862f7827e78d3f6ba0f4c7942f4d5a5685f4098e
Otávio <113644590+otavio61@users.noreply.github.com>
2025-11-13 00:44:21 -0300

    Refatoração de TalhaoDAO
    Testagem dos métodos do DAO através de TalhaoControle

--- src/br/edu/ifsp/hto/planejamento/modelo/tests/TalhaoDAOTest.java
+++ src/br/edu/ifsp/hto/planejamento/modelo/tests/TalhaoDAOTest.java
@@ -2,23 +2,23 @@ package br.edu.ifsp.hto.planejamento.modelo.tests;
 
 import java.util.List;
 
-import br.edu.ifsp.hto.planejamento.modelo.DAO.TalhaoDAO;
+import br.edu.ifsp.hto.planejamento.controle.TalhaoControle;
 import br.edu.ifsp.hto.planejamento.modelo.VO.TalhaoComPlanosVO;
 import br.edu.ifsp.hto.planejamento.modelo.VO.TalhaoVO;
 
 public class TalhaoDAOTest {
     public static void main(String[] args) {
-        TalhaoDAO talhaoDAO = new TalhaoDAO();
-        List<TalhaoVO> talhoes = talhaoDAO.listarTodos();
+        TalhaoControle talhaoC = new TalhaoControle();
+        List<TalhaoVO> talhoes = talhaoC.listarTodos();
         talhoes.forEach(talhao -> System.out.println(talhao.getNome()));
 
         TalhaoVO novo = new TalhaoVO(3, 3, "Talhão Daora", 400, "muito daora", "sem nada");
-        // talhaoDAO.inserir(novo);
+        // talhaoC.inserir(novo);
 
-        TalhaoComPlanosVO tp = talhaoDAO.buscarTalhaoComPlanos(1);
+        TalhaoComPlanosVO tp = talhaoC.buscarTalhaoComPlanos(1);
         tp.getPlanos().forEach(plano -> System.out.println("PLANO: " + plano.getNomePlano()));
 
-        talhaoDAO.deletar(2);
+        // talhaoC.deletar(2);
     }
 
 }
